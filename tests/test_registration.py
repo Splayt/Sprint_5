1c4288b7f9593c249ad1df15d7ea0df471df4399
Egor <egor0908@gmail.com>
2026-01-29 20:48:22 +0300

    Исправления

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,14 +5,20 @@ from config.urls import BASE_URL
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from mock_data import (
+    generate_email,
+    VALID_PASSWORD,
+    INVALID_EMAIL,
+    EXISTING_ACC
+)
 
 
 class TestRegistration:
 
     def test_reg_success(self, driver):
-        email = f'user_{random.randint(100000, 999999)}@mail.ru'
-        password = 'Password123'
-        expected_url = f'{BASE_URL}regiatration'
+        email = generate_email()
+        password = VALID_PASSWORD
+        expected_url = f'{BASE_URL}registration'
 
         driver.find_element(*locators.BTN_ENTER_REG).click()
         driver.find_element(*locators.BTN_NO_ACC).click()
@@ -30,59 +36,55 @@ class TestRegistration:
 
 
     def test_reg_invalid_email_fail(self, driver):
-        email = 'bad_email'
-
-
         driver.find_element(*locators.BTN_ENTER_REG).click()
         driver.find_element(*locators.BTN_NO_ACC).click()
 
-        driver.find_element(*locators.INPUT_EMAIL).send_keys(email)
-
+        driver.find_element(*locators.INPUT_EMAIL).send_keys(INVALID_EMAIL)
         driver.find_element(*locators.BTN_CREATE_ACC).click()
 
         wait = WebDriverWait(driver, 10)
 
-        email_wrap = wait.until(EC.presence_of_element_located(locators.EMAIL_ERROR_WRAP))
-        pwd_wrap = wait.until(EC.presence_of_element_located(locators.PASSWORD_ERROR_WRAP))
-        rep_wrap = wait.until(EC.presence_of_element_located(locators.REPEAT_PASSWORD_ERROR_WRAP))
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.EMAIL_ERROR_WRAP)
+        ).get_attribute('class')
 
-        error_text = wait.until(EC.presence_of_element_located(locators.REG_ERROR_TEXT))
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.PASSWORD_ERROR_WRAP)
+        ).get_attribute('class')
 
-        email_class = email_wrap.get_attribute('class')
-        pw_class = pwd_wrap.get_attribute('class')
-        pw_repeat_class = rep_wrap.get_attribute('class')
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.REPEAT_PASSWORD_ERROR_WRAP)
+        ).get_attribute('class')
 
-        assert 'input_inputError' in email_class
-        assert 'input_inputError' in pw_class
-        assert 'input_inputError' in pw_repeat_class
-        assert error_text.text == 'Ошибка'
+        assert wait.until(
+            EC.visibility_of_element_located(locators.REG_ERROR_TEXT)
+        )
 
     def test_reg_existing_acc_no_account_fail(self, driver):
-        email = 'm@mmmm.mm'
-        password = 'Mmmmm123'
-
         driver.find_element(*locators.BTN_ENTER_REG).click()
         driver.find_element(*locators.BTN_NO_ACC).click()
 
-        driver.find_element(*locators.INPUT_EMAIL).send_keys(email)
-        driver.find_element(*locators.INPUT_PASSWORD).send_keys(password)
-        driver.find_element(*locators.INPUT_REPEAT_PASSWORD).send_keys(password)
+        driver.find_element(*locators.INPUT_EMAIL).send_keys(EXISTING_ACC['email'])
+        driver.find_element(*locators.INPUT_PASSWORD).send_keys(EXISTING_ACC['password'])
+        driver.find_element(*locators.INPUT_REPEAT_PASSWORD).send_keys(EXISTING_ACC['password'])
         driver.find_element(*locators.BTN_CREATE_ACC).click()
 
         wait = WebDriverWait(driver, 10)
 
-        email_wrap = wait.until(EC.presence_of_element_located(locators.EMAIL_ERROR_WRAP))
-        pwd_wrap = wait.until(EC.presence_of_element_located(locators.PASSWORD_ERROR_WRAP))
-        rep_wrap = wait.until(EC.presence_of_element_located(locators.REPEAT_PASSWORD_ERROR_WRAP))
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.EMAIL_ERROR_WRAP)
+        ).get_attribute('class')
 
-        error_text = wait.until(EC.presence_of_element_located(locators.REG_ERROR_TEXT))
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.PASSWORD_ERROR_WRAP)
+        ).get_attribute('class')
 
-        email_class = email_wrap.get_attribute('class')
-        pw_class = pwd_wrap.get_attribute('class')
-        pw_repeat_class = rep_wrap.get_attribute('class')
+        assert 'input_inputError' in wait.until(
+            EC.presence_of_element_located(locators.REPEAT_PASSWORD_ERROR_WRAP)
+        ).get_attribute('class')
 
-        assert 'input_inputError' in email_class
-        assert 'input_inputError' in pw_class
-        assert 'input_inputError' in pw_repeat_class
-        assert error_text.text == 'Ошибка'
+        assert wait.until(
+            EC.visibility_of_element_located(locators.REG_ERROR_TEXT)
+        )
+
 
